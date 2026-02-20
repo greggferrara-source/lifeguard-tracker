0357c2829a20b48372b72f69f5a5475e3639778d
Gregory Ferrara <greggferrara@gmail.com>
2026-02-20 22:44:28 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { createPageUrl } from "./utils";
+import MobileLayout from "@/components/MobileLayout";
 import {
   LayoutDashboard,
   CalendarDays,
